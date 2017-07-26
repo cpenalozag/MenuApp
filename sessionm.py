8f71bf789cfaeeccd7d3a03895851289d68df3cb
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-07-25 22:36:04 -0500

    DB ready

---
+++ sessionm.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine=create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session=DBSession()
+restaurants=session.query(Restaurant).all()
+for restaurant in restaurants:
+    print (restaurant.name)
